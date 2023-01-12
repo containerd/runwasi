b25b6097053b74439fb6e354a5668d8609647b92
vincent <vincent@secondstate.io>
2023-01-12 14:08:58 +0800

    Move third party code into separate module
    Clean code
    
    Signed-off-by: vincent <vincent@secondstate.io>

--- crates/containerd-shim-wasm/src/sandbox/shim.rs
+++ crates/containerd-shim-wasm/src/sandbox/shim.rs
@@ -9,6 +9,8 @@ use std::sync::mpsc::{channel, Receiver, Sender};
 use std::sync::{Arc, Condvar, Mutex, RwLock};
 use std::thread;
 
+use super::instance::{EngineGetter, Instance, InstanceConfig, Nop};
+use super::{oci, Error, SandboxService};
 use chrono::{DateTime, Utc};
 use containerd_shim::{
     self as shim, api,
@@ -30,11 +32,9 @@ use nix::mount::{mount, MsFlags};
 use nix::sched::{setns, unshare, CloneFlags};
 use nix::sys::stat::Mode;
 use nix::unistd::mkdir;
-use oci_spec::runtime::{self, Mount};
+use oci_spec::runtime;
+use thirdparty::parse_mount;
 use ttrpc::context::Context;
-
-use super::instance::{EngineGetter, Instance, InstanceConfig, Nop};
-use super::{oci, Error, SandboxService};
 
 type InstanceDataStatus = (Mutex<Option<(u32, DateTime<Utc>)>>, Condvar);
 
@@ -1426,57 +1426,6 @@ where
             ..Default::default()
         })
     }
-}
-
-pub fn parse_mount(m: &Mount) -> MsFlags {
-    let mut flags = MsFlags::empty();
-    if let Some(options) = &m.options() {
-        for s in options {
-            if let Some((is_clear, flag)) = match s.as_str() {
-                "defaults" => Some((false, MsFlags::empty())),
-                "ro" => Some((false, MsFlags::MS_RDONLY)),
-                "rw" => Some((true, MsFlags::MS_RDONLY)),
-                "suid" => Some((true, MsFlags::MS_NOSUID)),
-                "nosuid" => Some((false, MsFlags::MS_NOSUID)),
-                "dev" => Some((true, MsFlags::MS_NODEV)),
-                "nodev" => Some((false, MsFlags::MS_NODEV)),
-                "exec" => Some((true, MsFlags::MS_NOEXEC)),
-                "noexec" => Some((false, MsFlags::MS_NOEXEC)),
-                "sync" => Some((false, MsFlags::MS_SYNCHRONOUS)),
-                "async" => Some((true, MsFlags::MS_SYNCHRONOUS)),
-                "dirsync" => Some((false, MsFlags::MS_DIRSYNC)),
-                "remount" => Some((false, MsFlags::MS_REMOUNT)),
-                "mand" => Some((false, MsFlags::MS_MANDLOCK)),
-                "nomand" => Some((true, MsFlags::MS_MANDLOCK)),
-                "atime" => Some((true, MsFlags::MS_NOATIME)),
-                "noatime" => Some((false, MsFlags::MS_NOATIME)),
-                "diratime" => Some((true, MsFlags::MS_NODIRATIME)),
-                "nodiratime" => Some((false, MsFlags::MS_NODIRATIME)),
-                "bind" => Some((false, MsFlags::MS_BIND)),
-                "rbind" => Some((false, MsFlags::MS_BIND | MsFlags::MS_REC)),
-                "unbindable" => Some((false, MsFlags::MS_UNBINDABLE)),
-                "runbindable" => Some((false, MsFlags::MS_UNBINDABLE | MsFlags::MS_REC)),
-                "private" => Some((true, MsFlags::MS_PRIVATE)),
-                "rprivate" => Some((true, MsFlags::MS_PRIVATE | MsFlags::MS_REC)),
-                "shared" => Some((true, MsFlags::MS_SHARED)),
-                "rshared" => Some((true, MsFlags::MS_SHARED | MsFlags::MS_REC)),
-                "slave" => Some((true, MsFlags::MS_SLAVE)),
-                "rslave" => Some((true, MsFlags::MS_SLAVE | MsFlags::MS_REC)),
-                "relatime" => Some((true, MsFlags::MS_RELATIME)),
-                "norelatime" => Some((true, MsFlags::MS_RELATIME)),
-                "strictatime" => Some((true, MsFlags::MS_STRICTATIME)),
-                "nostrictatime" => Some((true, MsFlags::MS_STRICTATIME)),
-                _ => None,
-            } {
-                if is_clear {
-                    flags &= !flag;
-                } else {
-                    flags |= flag;
-                }
-            };
-        }
-    }
-    flags
 }
 
 fn forward_events(
