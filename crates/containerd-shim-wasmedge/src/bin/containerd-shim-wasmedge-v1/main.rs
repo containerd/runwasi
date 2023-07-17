8091589d3340b973843fd5ff0953dbe466af5242
vincent <vincent@secondstate.io>
2023-07-17 13:11:21 +0800

    chore: bump wasmedge to 0.13.1 and wasmedge-sdk to 0.9.0 (#178)
    Signed-off-by: vincent <vincent@secondstate.io>

--- crates/containerd-shim-wasmedge/src/bin/containerd-shim-wasmedge-v1/main.rs
+++ crates/containerd-shim-wasmedge/src/bin/containerd-shim-wasmedge-v1/main.rs
@@ -3,5 +3,8 @@ use containerd_shim_wasm::sandbox::ShimCli;
 use containerd_shim_wasmedge::instance::Wasi as WasiInstance;
 
 fn main() {
-    shim::run::<ShimCli<WasiInstance, wasmedge_sdk::Vm>>("io.containerd.wasmedge.v1", None);
+    shim::run::<ShimCli<WasiInstance, wasmedge_sdk::Vm<wasmedge_sdk::NeverType>>>(
+        "io.containerd.wasmedge.v1",
+        None,
+    );
 }
