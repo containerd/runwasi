460967d2fc802f5d815911326d24eb05a84c7cba
jiaxiao zhou <jiazho@microsoft.com>
2023-08-01 20:13:07 +0000

    feat: remove cgroups implementation
    This commit removes the cgroups implementation in containerd-shim-wasm as
    the container runtimes are now using youki's libcontainer.
    
    Signed-off-by: jiaxiao zhou <jiazho@microsoft.com>

--- crates/containerd-shim-wasm/src/sandbox/oci.rs
+++ crates/containerd-shim-wasm/src/sandbox/oci.rs
@@ -3,7 +3,6 @@
 use std::fs::File;
 use std::path::{Path, PathBuf};
 
-use super::cgroups;
 use super::error::Result;
 use anyhow::Context;
 use nix::{sys::signal, unistd::Pid};
@@ -40,53 +39,6 @@ pub fn spec_from_file<P: AsRef<Path>>(path: P) -> Result<Spec> {
     let file = File::open(path)?;
     let cfg: Spec = json::from_reader(file)?;
     Ok(cfg)
-}
-
-struct NopCgroup {}
-
-impl cgroups::Cgroup for NopCgroup {
-    fn add_task(&self, _pid: u32) -> Result<()> {
-        Ok(())
-    }
-
-    fn version(&self) -> cgroups::Version {
-        cgroups::Version::V1
-    }
-
-    fn apply(&self, _res: Option<cgroups::Resources>) -> Result<()> {
-        Ok(())
-    }
-
-    fn delete(&self) -> Result<()> {
-        Ok(())
-    }
-}
-
-pub fn get_cgroup(spec: &Spec) -> Result<Box<dyn cgroups::Cgroup>> {
-    let linux = spec.linux();
-    if linux.is_none() {
-        return Ok(Box::new(NopCgroup {}));
-    }
-
-    match linux.as_ref().unwrap().cgroups_path() {
-        None => Ok(Box::new(NopCgroup {})),
-        Some(p) => cgroups::new(p.display().to_string()),
-    }
-}
-
-pub fn setup_cgroup(cg: &dyn cgroups::Cgroup, spec: &Spec) -> Result<()> {
-    if let Some(linux) = spec.linux() {
-        if let Some(res) = linux.resources() {
-            cg.apply(Some(res.clone())).map_err(|e| {
-                super::Error::Others(format!(
-                    "error applying cgroup settings from oci spec: cgroup version {}: {}",
-                    cg.version(),
-                    e
-                ))
-            })?;
-        }
-    }
-    Ok(())
 }
 
 fn parse_env(envs: &[String]) -> HashMap<String, String> {
