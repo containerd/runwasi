268aa67fab050b69bf729cd64d6609a4f3afce09
Jorge Prendes <jorge.prendes@gmail.com>
2023-08-24 16:27:56 +0100

    save time reclaiming less space
    Signed-off-by: Jorge Prendes <jorge.prendes@gmail.com>

--- scripts/setup-linux.sh
+++ scripts/setup-linux.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 sudo apt -y update
 sudo apt install -y pkg-config libsystemd-dev libdbus-glib-1-dev build-essential libelf-dev libseccomp-dev libclang-dev
+
+if [ ! -z "$CI" ] && ! mount | grep cgroup; then
+    echo "cgroup is not mounted" 1>&2
+    exit 1
+fi
