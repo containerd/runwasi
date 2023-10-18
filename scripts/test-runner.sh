217800a3bc756d2f693206db0eb09bcdb35e30ff
Jorge Prendes <jorge.prendes@gmail.com>
2023-10-18 15:04:17 +0100

    Use cross for building in CI
    Signed-off-by: Jorge Prendes <jorge.prendes@gmail.com>

--- scripts/test-runner.sh
+++ scripts/test-runner.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-sudo -E env LD_LIBRARY_PATH=${LD_LIBRARY_PATH} "$@"
+echo $@
+TARGET_DIR="$(dirname $0)/.."
+WASMEDGE_PATH=$(dirname $(find "$TARGET_DIR" -name libwasmedge.so | head -n 1) 2>/dev/null || echo "")
+sudo -E env LD_LIBRARY_PATH="${WASMEDGE_PATH}" "$@"
