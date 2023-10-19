19b96d2caa37e8730062035becc9ed46fa739630
Jorge Prendes <jorge.prendes@gmail.com>
2023-10-19 16:11:12 +0100

    use cross for release builds
    Signed-off-by: Jorge Prendes <jorge.prendes@gmail.com>

--- scripts/setup-cross.sh
+++ scripts/setup-cross.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 cargo install cross --git https://github.com/cross-rs/cross
-echo "CARGO=cross" >> ${GITHUB_ENV}
 
-if [ ! -z "$1" ]; then
-    echo "TARGET=$1" >> ${GITHUB_ENV}
+if [ ! -z "$CI" ]; then
+
+    echo "CARGO=cross" >> ${GITHUB_ENV}
+
+    if [ ! -z "$1" ]; then
+        echo "TARGET=$1" >> ${GITHUB_ENV}
+    fi
+
 fi
