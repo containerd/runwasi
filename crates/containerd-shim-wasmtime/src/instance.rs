1c8b2447b30d2d6c9c93aae5bf592c89200bf1fd
jiaxiao zhou <jiazho@microsoft.com>
2023-11-16 04:51:13 +0000

    feat(wasmtime): support wasm components
    Signed-off-by: jiaxiao zhou <jiazho@microsoft.com>

--- crates/containerd-shim-wasmtime/src/instance.rs
+++ crates/containerd-shim-wasmtime/src/instance.rs
@@ -3,14 +3,66 @@ use containerd_shim_wasm::container::{
     Engine, Entrypoint, Instance, PathResolve, RuntimeContext, Source, Stdio,
 };
 use wasi_common::I32Exit;
-use wasmtime::{Linker, Module, Store};
+use wasmtime::component::Component;
+use wasmtime::{Module, Store};
+use wasmtime_wasi::preview2::{self as wasi_preview2, Table};
 use wasmtime_wasi::{Dir, WasiCtxBuilder};
 
 pub type WasmtimeInstance = Instance<WasmtimeEngine>;
 
-#[derive(Clone, Default)]
+#[derive(Clone)]
 pub struct WasmtimeEngine {
     engine: wasmtime::Engine,
+}
+
+impl Default for WasmtimeEngine {
+    fn default() -> Self {
+        let mut config = wasmtime::Config::new();
+        config.wasm_component_model(true);
+        Self {
+            engine: wasmtime::Engine::new(&config)
+                .context("failed to create wasmtime engine")
+                .unwrap(),
+        }
+    }
+}
+
+pub struct Data {
+    wasi: wasi_preview2::WasiCtx,
+    table: Table,
+}
+
+impl wasmtime_wasi::preview2::WasiView for Data {
+    fn table(&self) -> &Table {
+        &self.table
+    }
+
+    fn table_mut(&mut self) -> &mut Table {
+        &mut self.table
+    }
+
+    fn ctx(&self) -> &wasi_preview2::WasiCtx {
+        &self.wasi
+    }
+
+    fn ctx_mut(&mut self) -> &mut wasi_preview2::WasiCtx {
+        &mut self.wasi
+    }
+}
+
+pub enum WasmBinaryType {
+    Module,
+    Component,
+}
+
+fn wasm_binary_type(bytes: &[u8]) -> Option<WasmBinaryType> {
+    if bytes.starts_with(b"\0asm\x01\0\0\0") {
+        return Some(WasmBinaryType::Module);
+    }
+    if bytes.starts_with(b"\0asm\x0d\0\x01\0") {
+        return Some(WasmBinaryType::Component);
+    }
+    None
 }
 
 impl Engine for WasmtimeEngine {
@@ -22,19 +74,6 @@ impl Engine for WasmtimeEngine {
         log::info!("setting up wasi");
         let root_path = Dir::from_std_file(std::fs::File::open("/")?);
         let envs: Vec<_> = std::env::vars().collect();
-
-        let mut wasi_builder = WasiCtxBuilder::new();
-        wasi_builder
-            .args(ctx.args())?
-            .envs(envs.as_slice())?
-            .inherit_stdio()
-            .preopened_dir(root_path, "/")?;
-
-        stdio.redirect()?;
-
-        log::info!("building wasi context");
-        let wctx = wasi_builder.build();
-
         let Entrypoint {
             source,
             func,
@@ -42,42 +81,101 @@ impl Engine for WasmtimeEngine {
             name: _,
         } = ctx.entrypoint();
 
-        log::info!("wasi context ready");
-        let module = match source {
+        let data = match source {
             Source::File(path) => {
-                log::info!("loading module from path {path:?}");
+                log::info!("loading module from file");
                 let path = path
                     .resolve_in_path_or_cwd()
                     .next()
                     .context("module not found")?;
-
-                Module::from_file(&self.engine, path)?
+                std::fs::read(path)?
             }
             Source::Oci([module]) => {
                 log::info!("loading module wasm OCI layers");
-                Module::from_binary(&self.engine, &module.layer)?
+                module.layer.clone()
             }
             Source::Oci(_modules) => {
                 bail!("only a single module is supported when using images with OCI layers")
             }
         };
 
-        let mut linker = Linker::new(&self.engine);
+        let status = match wasm_binary_type(&data) {
+            Some(WasmBinaryType::Module) => {
+                let module = Module::from_binary(&self.engine, &data)?;
+                let mut wasi_builder = WasiCtxBuilder::new();
+                wasi_builder
+                    .args(ctx.args())?
+                    .envs(envs.as_slice())?
+                    .inherit_stdio()
+                    .preopened_dir(root_path, "/")?;
 
-        wasmtime_wasi::add_to_linker(&mut linker, |s| s)?;
-        let mut store = Store::new(&self.engine, wctx);
+                stdio.redirect()?;
 
-        log::info!("instantiating instance");
-        let instance: wasmtime::Instance = linker.instantiate(&mut store, &module)?;
+                log::info!("building wasi context");
+                let wctx = wasi_builder.build();
 
-        log::info!("getting start function");
-        let start_func = instance
-            .get_func(&mut store, &func)
-            .context("module does not have a WASI start function")?;
+                let mut module_linker = wasmtime::Linker::new(&self.engine);
 
-        log::debug!("running with start function {func:?}");
+                wasmtime_wasi::add_to_linker(&mut module_linker, |s| s)?;
+                let mut store = Store::new(&self.engine, wctx);
 
-        let status = start_func.call(&mut store, &[], &mut []);
+                log::info!("instantiating instance");
+                let instance: wasmtime::Instance =
+                    module_linker.instantiate(&mut store, &module)?;
+
+                log::info!("getting start function");
+                let start_func = instance
+                    .get_func(&mut store, &func)
+                    .context("module does not have a WASI start function")?;
+
+                log::debug!("running start function {func:?}");
+
+                let status = start_func.call(&mut store, &[], &mut []);
+                status
+            }
+            Some(WasmBinaryType::Component) => {
+                let component = Component::from_binary(&self.engine, &data)?;
+                let file_perms = wasi_preview2::FilePerms::all();
+                let dir_perms = wasi_preview2::DirPerms::all();
+                let mut wasi_builder = wasi_preview2::WasiCtxBuilder::new();
+                wasi_builder
+                    .args(ctx.args())
+                    .envs(envs.as_slice())
+                    .inherit_stdio()
+                    .preopened_dir(root_path, dir_perms, file_perms, "/");
+
+                stdio.redirect()?;
+
+                log::info!("building wasi context");
+                let wctx = wasi_builder.build();
+
+                let data = Data {
+                    wasi: wctx,
+                    table: wasmtime_wasi::preview2::Table::new(),
+                };
+
+                let mut linker = wasmtime::component::Linker::new(&self.engine);
+
+                wasmtime_wasi::preview2::command::add_to_linker(&mut linker)?;
+                let mut store = Store::new(&self.engine, data);
+
+                log::info!("instantiating instance");
+                let instance: wasmtime::component::Instance =
+                    linker.instantiate(&mut store, &component)?;
+
+                log::info!("getting start function");
+                let start_func = instance
+                    .get_func(&mut store, &func)
+                    .context("module does not have a WASI start function")?;
+
+                log::debug!("running start function {func:?}");
+
+                let status = start_func.call(&mut store, &[], &mut []);
+                status
+            }
+            None => bail!("not a valid wasm binary format"),
+        };
+
         let status = status.map(|_| 0).or_else(|err| {
             match err.downcast_ref::<I32Exit>() {
                 // On Windows, exit status 3 indicates an abort (see below),
@@ -88,6 +186,7 @@ impl Engine for WasmtimeEngine {
                 _ => Err(err),
             }
         })?;
+        log::info!("wasi context ready");
 
         Ok(status)
     }
