ce3f30dfccaf3aace020c603ecefc78a2989accd
jiaxiao zhou <jiazho@microsoft.com>
2024-03-05 00:09:19 +0000

    feat(*): remove the network ns in the shim start
    this commit removes the unnecessary nerwork namepsace setup in the
    shim start function because it is already been set in youki
    
    Signed-off-by: jiaxiao zhou <jiazho@microsoft.com>

--- crates/containerd-shim-wasm/src/sandbox/shim/cli.rs
+++ crates/containerd-shim-wasm/src/sandbox/shim/cli.rs
@@ -14,6 +14,7 @@ use crate::sandbox::instance::Instance;
 use crate::sandbox::shim::events::{RemoteEventSender, ToTimestamp};
 use crate::sandbox::shim::local::Local;
 use crate::sys::networking::setup_namespaces;
+
 /// Cli implements the containerd-shim cli interface using `Local<T>` as the task service.
 pub struct Cli<T: Instance + Sync + Send> {
     engine: T::Engine,
@@ -68,9 +69,6 @@ where
             .as_ref()
             .and_then(|a| a.get("io.kubernetes.cri.sandbox-id"))
             .unwrap_or(&id);
-
-        setup_namespaces(&spec)
-            .map_err(|e| shim::Error::Other(format!("failed to setup namespaces: {}", e)))?;
 
         let (_child, address) = shim::spawn(opts, grouping, vec![])?;
 
