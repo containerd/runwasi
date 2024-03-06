3c1894c6131da8b7c888954fa31253c3d3ea77fe
James Sturtevant <jsturtevant@gmail.com>
2024-03-05 17:19:56 -0800

    Apply suggestions from code review
    Co-authored-by: Kate Goldenring <kate.goldenring@gmail.com>
    Signed-off-by: James Sturtevant <jsturtevant@gmail.com>

--- crates/containerd-shim-wasm/src/container/engine.rs
+++ crates/containerd-shim-wasm/src/container/engine.rs
@@ -54,11 +54,11 @@ pub trait Engine: Clone + Send + Sync + 'static {
         &["application/vnd.bytecodealliance.wasm.component.layer.v0+wasm"]
     }
 
-    /// Precompiles a passes supported OCI layers to engine for compilation
-    /// This is used to precompile the layers before they are run and will be called if can_precompile returns true.
+    /// Precompile passes supported OCI layers to engine for compilation
+    /// This is used to precompile the layers before they are run and will be called if `can_precompile` returns `true`.
     /// It is called only the first time a module is run and the resulting bytes will be cached in the containerd content store.  
     /// The cached, precompiled layers will be reloaded on subsequent runs.
-    /// The runtime is expected to runtime the same number of layers passed in, if the layer cannot be precompiled it should return None for that layer.
+    /// The runtime is expected to return the same number of layers passed in, if the layer cannot be precompiled it should return `None` for that layer.
     /// In some edge cases it is possible that the layers may already be precompiled and None should be returned in this case.
     fn precompile(&self, _layers: &[WasmLayer]) -> Result<Vec<Option<Vec<u8>>>> {
         bail!("precompile not supported");
