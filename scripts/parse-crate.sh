0f3fe7cc4fad1ff4e26d347bc2afaf8ba5e5a66a
jiaxiao zhou <jiazho@microsoft.com>
2024-04-16 23:52:34 +0000

    ci(*): remove signing from the build CI
    this commit removes signing from the build CI as the release
    pipeline will now handle signing of the binaries downloaded
    using actions/download-artifact.
    
    the biggest change is that the parse script is re-write to
    be able to output all the necessary variables such as
    `runtime`, `dry_run` and more.
    
    the action-sign workflow is also modified to take an extra
    `path` input to replace the hard-coded `dist/bin` path
    
    Signed-off-by: jiaxiao zhou <jiazho@microsoft.com>

--- scripts/parse-crate.sh
+++ scripts/parse-crate.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-crate_name=$1
+if [ -z "$1" ]; then
+    echo "Usage: $0 <path-to-main.log>"
+    exit 1
+fi
+
+log_file="$1"
+
+# extract crate and version from log file
+dry_run=false
+crate=$(grep 'Release ' "$log_file" | sed 's/.*Release \([a-zA-Z0-9_-]*\).*/\1/')
+version=$(grep 'Release ' "$log_file" | sed 's/.* v\(.*\)/\1/')
+if grep -q '\[dry-run\]' "$log_file"; then
+    dry_run=true
+fi
+
 is_binary="false"
 is_crate="false"
 
@@ -20,14 +34,27 @@ declare -A crate_map=(
 )
 
 # Check and assign based on the binary_map
-if [[ "${binary_map[$crate_name]}" == "true" ]]; then
+if [[ "${binary_map[$crate]}" == "true" ]]; then
     is_binary="true"
 fi
 
 # Check and assign based on the crate_map
-if [[ "${crate_map[$crate_name]}" == "true" ]]; then
+if [[ "${crate_map[$crate]}" == "true" ]]; then
     is_crate="true"
 fi
 
+# Runtime logic
+declare -a non_shim_crates=("containerd-shim-wasm" "containerd-shim-wasm-test-modules" "oci-tar-builder")
+runtime=""
+
+if printf '%s\n' "${non_shim_crates[@]}" | grep -q "^$crate$"; then
+    runtime="common"
+else
+    runtime="${crate#containerd-shim-}"
+fi
+echo "dry_run=$dry_run"
+echo "crate=$crate"
+echo "version=$version"
 echo "is_binary=$is_binary"
 echo "is_crate=$is_crate"
+echo "runtime=$runtime"
