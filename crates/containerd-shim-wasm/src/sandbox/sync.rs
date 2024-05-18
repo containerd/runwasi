678ad8f81fda071067a777f4c5de8fa1cbff4b46
jiaxiao zhou <jiazho@microsoft.com>
2024-05-18 01:42:28 +0000

    propogate tracing context for inter-process communication
    Signed-off-by: jiaxiao zhou <jiazho@microsoft.com>

--- crates/containerd-shim-wasm/src/sandbox/sync.rs
+++ crates/containerd-shim-wasm/src/sandbox/sync.rs
@@ -100,10 +100,6 @@ impl<T> WaitableCell<T> {
         let timeout = timeout.into();
         let cvar = &self.inner.cvar;
         let guard = self.inner.mutex.lock().unwrap();
-
-        let span = tracing::span!(tracing::Level::INFO, "wait_timeout");
-        let _enter = span.enter();
-
         let _guard = match timeout {
             None => cvar
                 .wait_while(guard, |_| self.inner.cell.get().is_none())
@@ -114,9 +110,6 @@ impl<T> WaitableCell<T> {
                 .map(|(guard, _)| guard)
                 .unwrap(),
         };
-
-        let _re_enter = span.enter();
-
         self.inner.cell.get()
     }
 }
