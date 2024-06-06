8b1113c57970a6351e0ee358a15048d0302d47e6
jiaxiao zhou <jiazho@microsoft.com>
2024-06-06 22:00:54 +0000

    feat(bench): Re-enable `cargo bench`
    This commit reverts back a change made by commit SHA 6882dd5bd7c1dba629bd97af5ede1c0a67b39c91 and fixed all the build errors for the benchmark.
    
    Closes #611
    
    Signed-off-by: jiaxiao zhou <jiazho@microsoft.com>

--- benches/containerd-shim-benchmarks/benches/webassembly-benchmarks.rs
+++ benches/containerd-shim-benchmarks/benches/webassembly-benchmarks.rs
@@ -1,27 +1,14 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-
 use std::borrow::Cow;
-use std::fs::{create_dir, File, OpenOptions};
-use std::io::Write;
-use std::os::unix::prelude::OpenOptionsExt;
-use std::path::{Path, PathBuf};
-use std::sync::mpsc::channel;
-use std::sync::Arc;
+use std::path::Path;
 use std::time::Duration;
 
 use anyhow::bail;
-use chrono::{DateTime, Utc};
-
-use containerd_shim_wasm::sandbox::exec::has_cap_sys_admin;
-use containerd_shim_wasm::sandbox::instance::Wait;
-use containerd_shim_wasm::sandbox::{Error, Instance, InstanceConfig};
-
-use containerd_shim_wasmedge::instance::{reset_stdio, Wasi as WasmEdgeWasi};
-use containerd_shim_wasmtime::instance::Wasi as WasmtimeWasi;
-use libc::SIGKILL;
-use oci_spec::runtime::{ProcessBuilder, RootBuilder, Spec, SpecBuilder};
-use serde::{Deserialize, Serialize};
-use tempfile::{tempdir, TempDir};
+use containerd_shim_wasm::container::Instance;
+use containerd_shim_wasm::sandbox::Error;
+use containerd_shim_wasm::testing::WasiTest;
+use containerd_shim_wasmedge::WasmEdgeInstance;
+use containerd_shim_wasmtime::instance::{DefaultConfig, WasmtimeEngine};
+use criterion::{criterion_group, criterion_main, Criterion};
 
 /*
     For benchmarking try to choose cases which run fast enough -- the idea is to
@@ -68,11 +55,8 @@ use tempfile::{tempdir, TempDir};
     of a longer benchmarking time). Running the whole suite on a desktop
     computer takes now a bit over 10 minutes.
 */
-
-#[derive(Serialize, Deserialize)]
-struct Options {
-    root: Option<PathBuf>,
-}
+type WasmtimeTestInstance = Instance<WasmtimeEngine<DefaultConfig>>;
+type WasmedgeTestInstance = WasmEdgeInstance;
 
 fn get_external_benchmark_module(name: String) -> Result<Vec<u8>, Error> {
     let manifest_dir = env!("CARGO_MANIFEST_DIR");
@@ -87,158 +71,54 @@ fn get_external_benchmark_module(name: String) -> Result<Vec<u8>, Error> {
     })
 }
 
-fn run_wasmtime_test_with_spec(
-    dir: &TempDir,
-    spec: &Spec,
-    wasmbytes: &[u8],
-) -> Result<(u32, DateTime<Utc>), Error> {
-    create_dir(dir.path().join("rootfs"))?;
-
-    let wasm_path = dir.path().join("rootfs/file.wasm");
-    let mut f = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .mode(0o755)
-        .open(wasm_path)?;
-    f.write_all(wasmbytes)?;
-
-    let stdout = File::create(dir.path().join("stdout"))?;
-    drop(stdout);
-
-    spec.save(dir.path().join("config.json"))?;
-
-    let mut cfg = InstanceConfig::new(WasmtimeWasi::new_engine()?, "test_namespace".into());
-    let cfg = cfg
-        .set_bundle(dir.path().to_str().unwrap().to_string())
-        .set_stdout(dir.path().join("stdout").to_str().unwrap().to_string());
-
-    let wasi = Arc::new(WasmtimeWasi::new("test".to_string(), Some(cfg)));
-
-    wasi.start()?;
-
-    let (tx, rx) = channel();
-    let waiter = Wait::new(tx);
-    wasi.wait(&waiter).unwrap();
-
-    let res = match rx.recv_timeout(Duration::from_secs(60)) {
-        Ok(res) => Ok(res),
-        Err(e) => {
-            wasi.kill(SIGKILL as u32).unwrap();
-            return Err(Error::Others(format!(
-                "error waiting for module to finish: {0}",
-                e
-            )));
-        }
-    };
-
-    wasi.delete()?;
-    res
+fn run_wasmtime_test_with_spec(wasmbytes: &[u8]) -> Result<u32, Error> {
+    let (exit_code, _, _) = WasiTest::<WasmtimeTestInstance>::builder()?
+        .with_wasm(wasmbytes)?
+        .build()?
+        .start()?
+        .wait(Duration::from_secs(10))?;
+    Ok(exit_code)
 }
 
-fn run_wasmedge_test_with_spec(
-    dir: &TempDir,
-    spec: &Spec,
-    wasmbytes: &[u8],
-) -> Result<(u32, DateTime<Utc>), Error> {
-    create_dir(dir.path().join("rootfs"))?;
-    let rootdir = dir.path().join("runwasi");
-    create_dir(&rootdir)?;
-    let opts = Options {
-        root: Some(rootdir),
-    };
-    let opts_file = OpenOptions::new()
-        .read(true)
-        .create(true)
-        .truncate(true)
-        .write(true)
-        .open(dir.path().join("options.json"))?;
-    write!(&opts_file, "{}", serde_json::to_string(&opts)?)?;
-
-    let wasm_path = dir.path().join("rootfs/file.wasm");
-    let mut f = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .mode(0o755)
-        .open(wasm_path)?;
-    f.write_all(wasmbytes)?;
-
-    let stdout = File::create(dir.path().join("stdout"))?;
-    drop(stdout);
-
-    spec.save(dir.path().join("config.json"))?;
-
-    let mut cfg = InstanceConfig::new(WasmEdgeWasi::new_engine()?, "test_namespace".into());
-    let cfg = cfg
-        .set_bundle(dir.path().to_str().unwrap().to_string())
-        .set_stdout(dir.path().join("stdout").to_str().unwrap().to_string());
-
-    let wasi = Arc::new(WasmEdgeWasi::new("test".to_string(), Some(cfg)));
-
-    wasi.start()?;
-
-    let (tx, rx) = channel();
-    let waiter = Wait::new(tx);
-    wasi.wait(&waiter).unwrap();
-
-    let res = match rx.recv_timeout(Duration::from_secs(600)) {
-        Ok(res) => Ok(res),
-        Err(e) => {
-            wasi.kill(SIGKILL as u32).unwrap();
-            return Err(Error::Others(format!(
-                "error waiting for module to finish: {0}",
-                e
-            )));
-        }
-    };
-    wasi.delete()?;
-    res
+fn run_wasmedge_test_with_spec(wasmbytes: &[u8]) -> Result<u32, Error> {
+    let (exit_code, _, _) = WasiTest::<WasmedgeTestInstance>::builder()?
+        .with_wasm(wasmbytes)?
+        .build()?
+        .start()?
+        .wait(Duration::from_secs(10))?;
+    Ok(exit_code)
 }
 
 fn run_bench(c: &mut Criterion, name: &str) -> Result<(), Error> {
-    if !has_cap_sys_admin() {
-        return Err(Error::Others(String::from(
-            "Please run the benchmark with sudo",
-        )));
-    }
-
     let wasmbytes = get_external_benchmark_module(format!("{}.wasm", name))?;
-
-    let spec = SpecBuilder::default()
-        .root(RootBuilder::default().path("rootfs").build()?)
-        .process(
-            ProcessBuilder::default()
-                .cwd("/")
-                .args(vec!["./file.wasm".to_string()])
-                .build()?,
-        )
-        .build()?;
 
     let bytes = Cow::from(wasmbytes);
 
     let mut group = c.benchmark_group(name);
     group.bench_function("Wasmtime", |b| {
         b.iter(|| {
-            let dir = tempdir().unwrap();
-            let res = run_wasmtime_test_with_spec(&dir, &spec, &bytes);
+            let res = run_wasmtime_test_with_spec(&bytes);
             match res {
                 Err(e) => bail!("Error running Wasmtime benchmark: {}", e),
-                _ => Ok(()),
+                Ok(status) => {
+                    assert_eq!(status, 0);
+                    Ok(())
+                }
             }
         })
     });
     group.bench_function("WasmEdge", |b| {
         b.iter(|| {
-            let dir = tempdir().unwrap();
-            let res = run_wasmedge_test_with_spec(&dir, &spec, &bytes);
+            let res = run_wasmedge_test_with_spec(&bytes);
             match res {
                 Err(e) => bail!("Error running WasmEdge benchmark: {}", e),
-                _ => Ok(()),
+                Ok(status) => {
+                    assert_eq!(status, 0);
+                    Ok(())
+                }
             }
         })
     });
-    reset_stdio();
     group.finish();
 
     Ok(())
@@ -354,7 +234,7 @@ fn bench_stream4(b: &mut Criterion) {
 
 criterion_group! {
     name = benches;
-    config = Criterion::default().sample_size(10);
+    config = Criterion::default().sample_size(10).measurement_time(Duration::from_secs(100));
     targets = bench_aead_chacha20poly13052, bench_aead_chacha20poly1305, bench_aead_xchacha20poly1305, bench_auth2, bench_auth3, bench_auth6, bench_auth, bench_box_seed, bench_generichash2, bench_generichash3, bench_hash3, bench_hash, bench_kdf, bench_keygen, bench_onetimeauth2, bench_onetimeauth, bench_scalarmult2, bench_scalarmult5, bench_scalarmult6, bench_secretbox2, bench_secretbox_easy, bench_secretbox, bench_secretstream_xchacha20poly1305, bench_shorthash, bench_siphashx24, bench_stream3, bench_stream4
 }
 
