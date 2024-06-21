133142cc29fe4c04a07255e341a374aa07243fc1
James Sturtevant <jstur@microsoft.com>
2024-06-21 22:36:17 +0000

    Add ability to generate new artifact type
    Signed-off-by: James Sturtevant <jstur@microsoft.com>

--- crates/oci-tar-builder/README.md
+++ crates/oci-tar-builder/README.md
@@ -22,7 +22,7 @@ See [wasi-demo-app build script](../wasi-demo-app/build.rs) for an example.
 
 There is an experimental executable that uses the library and can package a wasm module as an OCI image with wasm layers.  See the [OCI WASM in containerd](https://docs.google.com/document/d/11shgC3l6gplBjWF1VJCWvN_9do51otscAm0hBDGSSAc) for more information.
 
-To generate the package and import to a registry using a tool such as [regctl](https://github.com/regclient/regclient/blob/main/docs/regctl.md#image-commands): 
+To generate the package and import to a registry using a tool such as [regctl](https://github.com/regclient/regclient/blob/main/docs/install.md).  To [run a local image registry](https://www.docker.com/blog/how-to-use-your-own-registry-2/) use `docker run -d -p 5000:5000 --name registry registry:2.7`
 
 ```
 cargo run --bin oci-tar-builder -- --name wasi-demo-oci --repo ghcr.io/containerd/runwasi --tag latest --module ./target/wasm32-wasi/debug/wasi-demo-app.wasm -o ./dist/img-oci.tar
@@ -57,3 +57,53 @@ Layers:
 See the [OCI Image Spec](https://github.com/opencontainers/image-spec/blob/bc9c4bd/image-layout.md) for more information on the OCI tar format.
 
 In order to be compatible with Docker, since Docker does not currently support the OCI format, this also includes a `manifest.json` file at the root of the tar that describes the image in a way that Docker can import it.
+
+### Wasm Artifact usage
+
+The CNCF wg-wasm has published and [OCI artifact format](https://tag-runtime.cncf.io/wgs/wasm/deliverables/wasm-oci-artifact/) for packaging wasm modules and components.  The artifact can be produced locally by running the `--as-artifact` flag:
+
+```
+cargo run --bin oci-tar-builder -- --name wasi-demo-oci --repo ghcr.io/containerd/runwasi --tag latest --as-artifact --module ./target/wasm32-wasi/debug/wasi-demo-app.wasm -o target/wasm32-wasi/debug/img-oci-artifact.tar
+regctl image import localhost:5000/wasi-artifact:latest target/wasm32-wasi/debug/img-oci-artifact.tar
+```
+
+The manifest will follow the guidance:
+
+```
+regctl manifest get localhost:5000/wasi-artifact:latest
+
+Name:                                localhost:5000/wasi-artifact:latest
+MediaType:                           application/vnd.oci.image.manifest.v1+json
+Digest:                              sha256:7c31e635b3bef8b6c727a316e9a2dae777dbd184318d66a97da040fb11e37d70
+Annotations:
+  io.containerd.image.name:          ghcr.io/containerd/runwasi/wasi-demo-oci:latest
+  org.opencontainers.image.ref.name: latest
+Total Size:                          2.006MB
+
+Config:
+  Digest:                            sha256:24f30be41b447bbaf3644dad1e1c23dd28b597f36a5f455399657d65945816ea
+  MediaType:                         application/vnd.wasm.config.v0+json
+  Size:                              235B
+
+Layers:
+
+  Digest:                            sha256:0db51ed1c94837f422b2259c473758f298eef69605eaae6195bc043e25971e94
+  MediaType:                         application/wasm
+  Size:                              2.006MB
+```
+
+As well as the `config.mediaType` will have the following format:
+
+```
+ regctl blob get localhost:5000/wasi-artifact:latest sha256:24f30be41b447bbaf3644dad1e1c23dd28b597f36a5f455399657d65945816ea
+{
+  "created": "2024-06-25T15:58:49.377917735Z",
+  "author": null,
+  "architecture": "wasm",
+  "os": "wasip1",
+  "layerDigests": [
+    "sha256:0db51ed1c94837f422b2259c473758f298eef69605eaae6195bc043e25971e94"
+  ],
+  "component": null
+}
+```
--- crates/oci-tar-builder/src/bin.rs
+++ crates/oci-tar-builder/src/bin.rs
@@ -5,11 +5,13 @@ use std::{env, fs};
 
 use anyhow::Context;
 use clap::Parser;
-use oci_spec::image::{self as spec, Arch};
-use oci_tar_builder::{Builder, WASM_LAYER_MEDIA_TYPE};
+use oci_spec::image::{self as spec, Arch, ImageConfiguration};
+use oci_tar_builder::Builder;
+use oci_wasm::WasmConfig;
 use sha256::{digest, try_digest};
 
-pub fn main() {
+#[tokio::main]
+pub async fn main() {
     let args = Args::parse();
 
     let out_dir;
@@ -20,18 +22,88 @@ pub fn main() {
         out_dir = env::current_dir().unwrap();
     }
 
+    if !args.module.is_empty() && args.components.is_some() {
+        println!("Mutually exclusive flags: module and components");
+        return;
+    }
+
+    if args.module.is_empty() && args.components.is_none() {
+        println!("Must supply module or components");
+        return;
+    }
+
+    if args.as_artifact {
+        generate_wasm_artifact(args, out_dir).await.unwrap();
+    } else {
+        generate_wasi_image(args, out_dir).unwrap();
+    }
+}
+
+async fn generate_wasm_artifact(args: Args, out_dir: PathBuf) -> Result<(), anyhow::Error> {
+    println!("Generating wasm artifact");
+
+    let mut builder = Builder::<WasmConfig>::default();
+
+    let (conf, path) = match args.components {
+        Some(path) => {
+            let paths = fs::read_dir(&path)?;
+            if paths.count() != 1 {
+                println!(
+                    "Currently only supports a single component file {:?}",
+                    &path
+                );
+            }
+            let (conf, _) = WasmConfig::from_component(&path, None).await?;
+            (conf, path)
+        }
+        None => {
+            let module_path = args.module.first().unwrap();
+            let (conf, _) = WasmConfig::from_module(module_path, None).await?;
+            (conf, module_path.to_string())
+        }
+    };
+
+    builder.add_config(
+        conf,
+        args.repo + "/" + &args.name + ":" + &args.tag,
+        spec::MediaType::Other(oci_wasm::WASM_MANIFEST_CONFIG_MEDIA_TYPE.to_string()),
+    );
+
+    let module_path = PathBuf::from(path);
+    builder.add_layer_with_media_type(&module_path, oci_wasm::WASM_LAYER_MEDIA_TYPE.to_string());
+
+    println!("Creating oci tar file {}", out_dir.clone().display());
+    let f = File::create(out_dir.clone())?;
+    match builder.build(f) {
+        Ok(_) => println!("Successfully created oci tar file {}", out_dir.display()),
+        Err(e) => {
+            print!(
+                "Building oci tar file {} failed: {:?}",
+                out_dir.display(),
+                e
+            );
+            fs::remove_file(out_dir).unwrap_or(print!("Failed to clean up oci tar file on error"));
+        }
+    }
+
+    Ok(())
+}
+
+fn generate_wasi_image(args: Args, out_dir: PathBuf) -> Result<(), anyhow::Error> {
+    println!("Generating wasm oci image");
     let entry_point = args.name.clone() + ".wasm";
 
-    let mut builder = Builder::default();
+    let mut builder = Builder::<ImageConfiguration>::default();
     let mut layer_digests = Vec::new();
     for module_path in args.module.iter() {
         let module_path = PathBuf::from(module_path);
-        builder.add_layer_with_media_type(&module_path, WASM_LAYER_MEDIA_TYPE.to_string());
-        layer_digests.push(
-            try_digest(&module_path)
-                .context("failed to calculate digest for module")
-                .unwrap(),
+        builder.add_layer_with_media_type(
+            &module_path,
+            oci_tar_builder::WASM_LAYER_MEDIA_TYPE.to_string(),
         );
+
+        layer_digests
+            .push(try_digest(&module_path).context("failed to calculate digest for module")?);
     }
 
     for layer_config in args.layer.iter() {
@@ -41,32 +113,33 @@ pub fn main() {
         let layer_type = layer_options.first().unwrap();
         let layer_path = PathBuf::from(layer_options.last().unwrap());
         builder.add_layer_with_media_type(&layer_path, layer_type.to_string());
-        layer_digests.push(
-            try_digest(&layer_path)
-                .context("failed to calculate digest for module")
-                .unwrap(),
-        );
+        layer_digests
+            .push(try_digest(&layer_path).context("failed to calculate digest for module")?);
     }
 
     if let Some(components_path) = args.components.as_deref() {
-        let paths = fs::read_dir(components_path).unwrap();
+        let paths = fs::read_dir(components_path)?;
 
         for path in paths {
-            let path = path.unwrap().path();
-            let ext = path.extension().unwrap().to_str().unwrap();
+            let path = path?.path();
+            let ext = path
+                .extension()
+                .unwrap_or(std::ffi::OsStr::new(""))
+                .to_str()
+                .unwrap_or("");
             match ext {
                 "wasm" => {
-                    builder.add_layer_with_media_type(&path, WASM_LAYER_MEDIA_TYPE.to_string());
-                    layer_digests.push(
-                        try_digest(&path)
-                            .context("failed to calculate digest for module")
-                            .unwrap(),
+                    builder.add_layer_with_media_type(
+                        &path,
+                        oci_tar_builder::WASM_LAYER_MEDIA_TYPE.to_string(),
                     );
+                    layer_digests
+                        .push(try_digest(&path).context("failed to calculate digest for module")?);
                 }
                 _ => println!(
                     "Skipping Unknown file type: {:?} with extension {:?}",
                     path,
-                    path.extension().unwrap()
+                    path.extension().unwrap_or(std::ffi::OsStr::new(""))
                 ),
             }
         }
@@ -77,27 +150,25 @@ pub fn main() {
     let unique_id = digest(layer_digests.join(""));
     let mut labels: HashMap<String, String> = HashMap::new();
     labels.insert("containerd.runwasi.layers".to_string(), unique_id);
+
     let config = spec::ConfigBuilder::default()
         .entrypoint(vec![entry_point])
         .labels(labels)
-        .build()
-        .unwrap();
+        .build()?;
 
-    let img = spec::ImageConfigurationBuilder::default()
+    let conf = spec::ImageConfigurationBuilder::default()
         .config(config)
         .os("wasip1")
         .architecture(Arch::Wasm)
-        .rootfs(
-            spec::RootFsBuilder::default()
-                .diff_ids(vec![])
-                .build()
-                .unwrap(),
-        )
+        .rootfs(spec::RootFsBuilder::default().diff_ids(vec![]).build()?)
         .build()
-        .context("failed to build image configuration")
-        .unwrap();
+        .context("failed to build image configuration")?;
 
-    builder.add_config(img, args.repo + "/" + &args.name + ":" + &args.tag);
+    builder.add_config(
+        conf,
+        format!("{}/{}:{}", args.repo, args.name, args.tag),
+        spec::MediaType::ImageConfig,
+    );
 
     println!("Creating oci tar file {}", out_dir.clone().display());
     let f = File::create(out_dir.clone()).unwrap();
@@ -112,6 +183,8 @@ pub fn main() {
             fs::remove_file(out_dir).unwrap_or(print!("Failed to remove temporary file"));
         }
     }
+
+    Ok(())
 }
 
 #[derive(Parser, Debug)]
@@ -137,4 +210,7 @@ struct Args {
 
     #[arg(short, long)]
     components: Option<String>,
+
+    #[arg(short, long)]
+    as_artifact: bool,
 }
