273e5ae40ba7e37a4ca9a3124ec9fb85782ca95e
Fawad Shaikh <fshaikh@gmail.com>
2024-09-30 04:03:22 +0000

    update test name to be clear

--- crates/containerd-shim-wasm/src/container/context.rs
+++ crates/containerd-shim-wasm/src/container/context.rs
@@ -426,7 +426,7 @@ mod tests {
     }
 
     #[test]
-    fn test_envs_not_present() -> Result<()> {
+    fn test_envs_return_default_only() -> Result<()> {
         let spec = SpecBuilder::default()
             .root(RootBuilder::default().path("rootfs").build()?)
             .process(ProcessBuilder::default().cwd("/").build()?)
