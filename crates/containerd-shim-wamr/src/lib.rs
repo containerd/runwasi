5030afa3475c44ad92a353db7fadf481a18d867c
jiaxiao zhou <duibao55328@gmail.com>
2024-11-03 22:51:38 +0000

    containerd-shim-wamr: added tests
    I believe there are more work to be done to parse the runtimeError
    to appropriate exit code.
    
    failures:
        wamr_tests::test_custom_entrypoint
        wamr_tests::test_exit_code
        wamr_tests::test_hello_world_oci
        wamr_tests::test_seccomp
        wamr_tests::test_unreachable
    
    Signed-off-by: jiaxiao zhou <duibao55328@gmail.com>

--- crates/containerd-shim-wamr/src/lib.rs
+++ crates/containerd-shim-wamr/src/lib.rs
@@ -1,3 +1,8 @@
 pub mod instance;
 
 pub use instance::WamrInstance;
+
+#[cfg(unix)]
+#[cfg(test)]
+#[path = "tests.rs"]
+mod wamr_tests;
---
+++ crates/containerd-shim-wamr/src/tests.rs
@@ -0,0 +1,118 @@
+use std::time::Duration;
+
+//use containerd_shim_wasm::sandbox::Instance;
+use containerd_shim_wasm::testing::modules::*;
+use containerd_shim_wasm::testing::WasiTest;
+use serial_test::serial;
+
+use crate::instance::WamrInstance as WasiInstance;
+
+#[test]
+#[serial]
+fn test_delete_after_create() -> anyhow::Result<()> {
+    WasiTest::<WasiInstance>::builder()?.build()?.delete()?;
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn test_hello_world() -> anyhow::Result<()> {
+    let (exit_code, stdout, _) = WasiTest::<WasiInstance>::builder()?
+        .with_wasm(HELLO_WORLD)?
+        .build()?
+        .start()?
+        .wait(Duration::from_secs(10))?;
+
+    assert_eq!(exit_code, 0);
+    assert_eq!(stdout, "hello world\n");
+
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn test_hello_world_oci() -> anyhow::Result<()> {
+    let (builder, _oci_cleanup) = WasiTest::<WasiInstance>::builder()?
+        .with_wasm(HELLO_WORLD)?
+        .as_oci_image(None, None)?;
+
+    let (exit_code, stdout, _) = builder.build()?.start()?.wait(Duration::from_secs(10))?;
+
+    assert_eq!(exit_code, 0);
+    assert_eq!(stdout, "hello world\n");
+
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn test_custom_entrypoint() -> anyhow::Result<()> {
+    let (exit_code, stdout, _) = WasiTest::<WasiInstance>::builder()?
+        .with_start_fn("foo")
+        .with_wasm(CUSTOM_ENTRYPOINT)?
+        .build()?
+        .start()?
+        .wait(Duration::from_secs(10))?;
+
+    assert_eq!(exit_code, 0);
+    assert_eq!(stdout, "hello world\n");
+
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn test_unreachable() -> anyhow::Result<()> {
+    let (exit_code, _, _) = WasiTest::<WasiInstance>::builder()?
+        .with_wasm(UNREACHABLE)?
+        .build()?
+        .start()?
+        .wait(Duration::from_secs(10))?;
+
+    assert_ne!(exit_code, 0);
+
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn test_exit_code() -> anyhow::Result<()> {
+    let (exit_code, _, _) = WasiTest::<WasiInstance>::builder()?
+        .with_wasm(EXIT_CODE)?
+        .build()?
+        .start()?
+        .wait(Duration::from_secs(10))?;
+
+    assert_eq!(exit_code, 42);
+
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn test_seccomp() -> anyhow::Result<()> {
+    let (exit_code, stdout, _) = WasiTest::<WasiInstance>::builder()?
+        .with_wasm(SECCOMP)?
+        .build()?
+        .start()?
+        .wait(Duration::from_secs(10))?;
+
+    assert_eq!(exit_code, 0);
+    assert_eq!(stdout.trim(), "current working dir: /");
+
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn test_has_default_devices() -> anyhow::Result<()> {
+    let (exit_code, _, _) = WasiTest::<WasiInstance>::builder()?
+        .with_wasm(HAS_DEFAULT_DEVICES)?
+        .build()?
+        .start()?
+        .wait(Duration::from_secs(10))?;
+
+    assert_eq!(exit_code, 0);
+
+    Ok(())
+}
