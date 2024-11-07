3046d3e60525f1fe6b39714f01eacb16abcb4c57
jiaxiao zhou <duibao55328@gmail.com>
2024-11-07 15:59:25 +0000

    containerd-shim-wamr: add conditional compilation for unix platform
    Signed-off-by: jiaxiao zhou <duibao55328@gmail.com>

--- crates/containerd-shim-wamr/src/lib.rs
+++ crates/containerd-shim-wamr/src/lib.rs
@@ -1,5 +1,7 @@
+#[cfg(unix)]
 pub mod instance;
 
+#[cfg(unix)]
 pub use instance::WamrInstance;
 
 #[cfg(unix)]
--- crates/containerd-shim-wamr/src/main.rs
+++ crates/containerd-shim-wamr/src/main.rs
@@ -1,6 +1,12 @@
 use containerd_shim_wamr::WamrInstance;
 use containerd_shim_wasm::sandbox::cli::{revision, shim_main, version};
 
+#[cfg(target_os = "windows")]
+fn main() {
+    compile_error!("This shim binary only supports Unix");
+}
+
+#[cfg(not(target_os = "windows"))]
 fn main() {
     shim_main::<WamrInstance>("wamr", version!(), revision!(), "v1", None);
 }
