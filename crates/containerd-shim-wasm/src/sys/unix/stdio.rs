9a5be0c79f15be1f6f720807e235d5a6998ed9a4
Jorge Prendes <jorge.prendes@gmail.com>
2025-01-02 12:09:35 +0000

    remove stdio functionalities
    Signed-off-by: Jorge Prendes <jorge.prendes@gmail.com>

--- crates/containerd-shim-wasm/src/sys/unix/stdio.rs
+++ crates/containerd-shim-wasm/src/sys/unix/stdio.rs
@@ -1,55 +1,7 @@
 use std::fs::{File, OpenOptions};
 use std::io::Result;
-use std::os::fd::{IntoRawFd, OwnedFd, RawFd};
 use std::path::Path;
-
-use crossbeam::atomic::AtomicCell;
-pub use libc::{STDERR_FILENO, STDIN_FILENO, STDOUT_FILENO};
-
-pub type StdioRawFd = RawFd;
-
-pub struct StdioOwnedFd(AtomicCell<StdioRawFd>);
 
 pub fn open(path: impl AsRef<Path>) -> Result<File> {
     OpenOptions::new().read(true).write(true).open(path)
-}
-
-impl Drop for StdioOwnedFd {
-    fn drop(&mut self) {
-        let fd = self.0.swap(-1);
-        if fd >= 0 {
-            unsafe { libc::close(fd) };
-        }
-    }
-}
-
-impl Default for StdioOwnedFd {
-    fn default() -> Self {
-        Self(AtomicCell::new(-1))
-    }
-}
-
-impl StdioOwnedFd {
-    pub fn try_from(f: impl Into<OwnedFd>) -> Result<Self> {
-        let fd = f.into().into_raw_fd();
-        Ok(unsafe { Self::from_raw_fd(fd) })
-    }
-
-    pub unsafe fn from_raw_fd(fd: StdioRawFd) -> Self {
-        Self(AtomicCell::new(fd))
-    }
-
-    pub fn as_raw_fd(&self) -> Option<StdioRawFd> {
-        let fd = self.0.load();
-        (fd >= 0).then_some(fd)
-    }
-
-    pub fn take(&self) -> Self {
-        let fd = self.0.swap(-1);
-        unsafe { Self::from_raw_fd(fd) }
-    }
-
-    pub fn try_from_path(path: impl AsRef<Path>) -> Result<Self> {
-        Self::try_from(open(path)?)
-    }
 }
--- crates/containerd-shim-wasm/src/sys/windows/stdio.rs
+++ crates/containerd-shim-wasm/src/sys/windows/stdio.rs
@@ -1,21 +1,9 @@
 use std::fs::{File, OpenOptions};
-use std::io::ErrorKind::Other;
-use std::io::{Error, Result};
-use std::os::windows::fs::OpenOptionsExt;
-use std::os::windows::prelude::{AsRawHandle, IntoRawHandle, OwnedHandle};
+use std::io::Result;
+use std::os::windows::fs::OpenOptionsExt as _;
 use std::path::Path;
 
-use crossbeam::atomic::AtomicCell;
-use libc::{intptr_t, open_osfhandle, O_APPEND};
 use windows_sys::Win32::Storage::FileSystem::FILE_FLAG_OVERLAPPED;
-
-pub type StdioRawFd = libc::c_int;
-
-pub const STDIN_FILENO: StdioRawFd = 0;
-pub const STDOUT_FILENO: StdioRawFd = 1;
-pub const STDERR_FILENO: StdioRawFd = 2;
-
-pub struct StdioOwnedFd(AtomicCell<StdioRawFd>);
 
 pub fn open(path: impl AsRef<Path>) -> Result<File> {
     // Containerd always passes a named pipe for stdin, stdout, and stderr so we can check if it is a pipe and open with overlapped IO
@@ -25,49 +13,4 @@ pub fn open(path: impl AsRef<Path>) -> Result<File> {
         options.custom_flags(FILE_FLAG_OVERLAPPED);
     }
     options.open(path)
-}
-
-impl Drop for StdioOwnedFd {
-    fn drop(&mut self) {
-        let fd = self.0.swap(-1);
-        if fd >= 0 {
-            unsafe { libc::close(fd) };
-        }
-    }
-}
-
-impl Default for StdioOwnedFd {
-    fn default() -> Self {
-        Self(AtomicCell::new(-1))
-    }
-}
-
-impl StdioOwnedFd {
-    pub fn try_from(f: impl Into<OwnedHandle>) -> Result<Self> {
-        let handle = f.into();
-        let fd = unsafe { open_osfhandle(handle.as_raw_handle() as intptr_t, O_APPEND) };
-        if fd == -1 {
-            return Err(Error::new(Other, "Failed to open file descriptor"));
-        }
-        let _ = handle.into_raw_handle(); // drop ownership of the handle, it's managed by fd now
-        Ok(unsafe { Self::from_raw_fd(fd) })
-    }
-
-    pub unsafe fn from_raw_fd(fd: StdioRawFd) -> Self {
-        Self(AtomicCell::new(fd))
-    }
-
-    pub fn as_raw_fd(&self) -> Option<StdioRawFd> {
-        let fd = self.0.load();
-        (fd >= 0).then_some(fd)
-    }
-
-    pub fn take(&self) -> Self {
-        let fd = self.0.swap(-1);
-        unsafe { Self::from_raw_fd(fd) }
-    }
-
-    pub fn try_from_path(path: impl AsRef<Path>) -> Result<Self> {
-        Self::try_from(open(path)?)
-    }
 }
