a1ad1fcfade86f6f3e6aec3ff1f940ad27ab9c4c
Jorge Prendes <jorge.prendes@gmail.com>
2025-01-02 12:33:27 +0000

    remove stdio from crate
    Signed-off-by: Jorge Prendes <jorge.prendes@gmail.com>

--- crates/containerd-shim-wasm/src/sandbox/mod.rs
+++ crates/containerd-shim-wasm/src/sandbox/mod.rs
@@ -5,13 +5,11 @@ pub mod error;
 pub mod instance;
 pub mod instance_utils;
 pub mod shim;
-pub mod stdio;
 pub mod sync;
 
 pub use error::{Error, Result};
 pub use instance::{Instance, InstanceConfig};
 pub use shim::Cli as ShimCli;
-pub use stdio::Stdio;
 
 pub(crate) mod containerd;
 pub(crate) mod oci;
