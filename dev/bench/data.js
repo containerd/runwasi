e7a798a5883feb8a86eb23172a36191478987df1
github-action-benchmark <github@users.noreply.github.com>
2025-01-05 00:18:04 +0000

    add HTTP Throughput (customBiggerIsBetter) benchmark result for 94751dc3921327bbbbcb1cd7cdc4a37eb6027a00

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1735949949213,
+  "lastUpdate": 1736036284384,
   "repoUrl": "https://github.com/containerd/runwasi",
   "entries": {
     "Criterion.rs Benchmark": [
@@ -6610,6 +6610,33 @@ window.BENCHMARK_DATA = {
           {
             "name": "HTTP RPS",
             "value": 20523.7073,
+            "unit": "req/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Jiaxiao Zhou",
+            "username": "Mossaka",
+            "email": "duibao55328@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "94751dc3921327bbbbcb1cd7cdc4a37eb6027a00",
+          "message": "Merge pull request #782 from jprendes/maker-wasmtime\n\nMake WasmtimeEngine a marker struct",
+          "timestamp": "2025-01-03T19:46:52Z",
+          "url": "https://github.com/containerd/runwasi/commit/94751dc3921327bbbbcb1cd7cdc4a37eb6027a00"
+        },
+        "date": 1736036283894,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "HTTP RPS",
+            "value": 20910.2988,
             "unit": "req/s"
           }
         ]
