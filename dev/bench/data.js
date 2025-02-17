253c5cf3b634b8e18a79321464727046e7b66179
github-action-benchmark <github@users.noreply.github.com>
2025-02-17 00:30:35 +0000

    add Criterion.rs Benchmark (customSmallerIsBetter) benchmark result for dfbc79a5dce63910f81d901effa226e874ccd845

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1739665230053,
+  "lastUpdate": 1739752235607,
   "repoUrl": "https://github.com/containerd/runwasi",
   "entries": {
     "Criterion.rs Benchmark": [
@@ -5346,6 +5346,52 @@ window.BENCHMARK_DATA = {
             "value": 82556709,
             "range": "± 1282338",
             "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Jiaxiao Zhou",
+            "username": "Mossaka",
+            "email": "duibao55328@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "dfbc79a5dce63910f81d901effa226e874ccd845",
+          "message": "Merge pull request #830 from Mossaka/stress-test-c8d\n\nfeat(stress-test): changes the stress-test crate to run workloads with Containerd",
+          "timestamp": "2025-02-14T21:44:40Z",
+          "url": "https://github.com/containerd/runwasi/commit/dfbc79a5dce63910f81d901effa226e874ccd845"
+        },
+        "date": 1739752234528,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "wamr/memory-usage",
+            "value": 14440,
+            "unit": "kB",
+            "extra": "shim: 11776 kB\nzygote: 2664 kB"
+          },
+          {
+            "name": "wasmedge/memory-usage",
+            "value": 64652,
+            "unit": "kB",
+            "extra": "shim: 52480 kB\nzygote: 12172 kB"
+          },
+          {
+            "name": "wasmer/memory-usage",
+            "value": 17328,
+            "unit": "kB",
+            "extra": "shim: 14208 kB\nzygote: 3120 kB"
+          },
+          {
+            "name": "wasmtime/memory-usage",
+            "value": 18636,
+            "unit": "kB",
+            "extra": "shim: 15488 kB\nzygote: 3148 kB"
           }
         ]
       }
