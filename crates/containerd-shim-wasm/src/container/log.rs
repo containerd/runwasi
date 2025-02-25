51b7d760d65c2c2eedf7835a5cfd64d7c85d88dc
Jiaxiao (mossaka) Zhou <duibao55328@gmail.com>
2025-02-25 22:42:16 +0000

    container: add pod id to the runtimeContext
    Signed-off-by: Jiaxiao (mossaka) Zhou <duibao55328@gmail.com>

--- crates/containerd-shim-wasm/src/container/log.rs
+++ crates/containerd-shim-wasm/src/container/log.rs
@@ -3,27 +3,57 @@
 macro_rules! log_with_ctx {
     // Info level log
     (info, $ctx:expr, $($arg:tt)+) => {
-        log::info!(instance = $ctx.container_id(); $($arg)+)
+        {
+            let ctx = $ctx;
+            match ctx.pod_id() {
+                Some(pod_id) => log::info!(instance = ctx.container_id(), pod = pod_id; $($arg)+),
+                None => log::info!(instance = ctx.container_id(); $($arg)+)
+            }
+        }
     };
 
     // Debug level log
     (debug, $ctx:expr, $($arg:tt)+) => {
-        log::debug!(instance = $ctx.container_id(); $($arg)+)
+        {
+            let ctx = $ctx;
+            match ctx.pod_id() {
+                Some(pod_id) => log::debug!(instance = ctx.container_id(), pod = pod_id; $($arg)+),
+                None => log::debug!(instance = ctx.container_id(); $($arg)+)
+            }
+        }
     };
 
     // Warn level log
     (warn, $ctx:expr, $($arg:tt)+) => {
-        log::warn!(instance = $ctx.container_id(); $($arg)+)
+        {
+            let ctx = $ctx;
+            match ctx.pod_id() {
+                Some(pod_id) => log::warn!(instance = ctx.container_id(), pod = pod_id; $($arg)+),
+                None => log::warn!(instance = ctx.container_id(); $($arg)+)
+            }
+        }
     };
 
     // Error level log
     (error, $ctx:expr, $($arg:tt)+) => {
-        log::error!(instance = $ctx.container_id(); $($arg)+)
+        {
+            let ctx = $ctx;
+            match ctx.pod_id() {
+                Some(pod_id) => log::error!(instance = ctx.container_id(), pod = pod_id; $($arg)+),
+                None => log::error!(instance = ctx.container_id(); $($arg)+)
+            }
+        }
     };
 
     // Trace level log
     (trace, $ctx:expr, $($arg:tt)+) => {
-        log::trace!(instance = $ctx.container_id(); $($arg)+)
+        {
+            let ctx = $ctx;
+            match ctx.pod_id() {
+                Some(pod_id) => log::trace!(instance = ctx.container_id(), pod = pod_id; $($arg)+),
+                None => log::trace!(instance = ctx.container_id(); $($arg)+)
+            }
+        }
     };
 }
 
