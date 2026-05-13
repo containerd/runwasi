715fb9cb2b47b836c1ee859e06c046d1f206fcaa
github-action-benchmark <github@users.noreply.github.com>
2026-05-13 00:43:16 +0000

    add Criterion.rs Benchmark (customSmallerIsBetter) benchmark result for b456f0bd301c8607dbaf5c0e73d76fdab1cad9f8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778547247434,
+  "lastUpdate": 1778632996001,
   "repoUrl": "https://github.com/containerd/runwasi",
   "entries": {
     "Criterion.rs Benchmark": [
@@ -57360,6 +57360,52 @@ window.BENCHMARK_DATA = {
             "value": 278035061,
             "range": "± 6420264",
             "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Brian Goff",
+            "username": "cpuguy83",
+            "email": "cpuguy83@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "b456f0bd301c8607dbaf5c0e73d76fdab1cad9f8",
+          "message": "README: clarify containerd sub-project status (#1124)\n\nThis was accidentally removed in https://github.com/containerd/runwasi/commit/1599a7809d2ee46ab6349dcef05822c63452d6ad\n\nSigned-off-by: Brian Goff <cpuguy83@gmail.com>",
+          "timestamp": "2026-05-12T09:17:06Z",
+          "url": "https://github.com/containerd/runwasi/commit/b456f0bd301c8607dbaf5c0e73d76fdab1cad9f8"
+        },
+        "date": 1778632974380,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "wamr/memory-usage",
+            "value": 15504,
+            "unit": "kB",
+            "extra": "shim: 12416 kB\nzygote: 3088 kB"
+          },
+          {
+            "name": "wasmedge/memory-usage",
+            "value": 66612,
+            "unit": "kB",
+            "extra": "shim: 54040 kB\nzygote: 12572 kB"
+          },
+          {
+            "name": "wasmer/memory-usage",
+            "value": 18916,
+            "unit": "kB",
+            "extra": "shim: 15376 kB\nzygote: 3540 kB"
+          },
+          {
+            "name": "wasmtime/memory-usage",
+            "value": 20520,
+            "unit": "kB",
+            "extra": "shim: 16964 kB\nzygote: 3556 kB"
           }
         ]
       }
